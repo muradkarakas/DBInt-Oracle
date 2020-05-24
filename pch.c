95ca88718bad351b30692803fe8c787b1d7cce4e
Murad Karakaş <muradkarakas@gmail.com>
2020-05-24 10:36:50 +0300

    Copyright message added

--- pch.c
+++ pch.c
@@ -1,6 +1,15 @@
-// stdafx.cpp : source file that includes just the standard includes
-// oracle-interface.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+/**
+ * This file is part of Sodium Language project
+ *
+ * Copyright © 2020 Murad Karakaþ <muradkarakas@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v3.0
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ *	https://choosealicense.com/licenses/gpl-3.0/
+ */
 
 #include "pch.h"
 
